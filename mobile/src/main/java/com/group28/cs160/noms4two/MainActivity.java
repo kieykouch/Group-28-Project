8fa0182569b2cd280103b16a590fb6ef7be20dec
Pradeep Kalipatnapu <prad.me@gmail.com>
2016-04-27 01:33:48 -0700

    Add a class the tracks nutrition information.

--- mobile/src/main/java/com/group28/cs160/noms4two/MainActivity.java
+++ mobile/src/main/java/com/group28/cs160/noms4two/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
@@ -20,11 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mFragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
 
-        mBottomBar = BottomBar.attach(this, savedInstanceState);
-        mBottomBar.useFixedMode();
-        mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+        bottomBar = bottomBar.attach(this, savedInstanceState);
+        bottomBar.useFixedMode();
+        bottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId) {
                 switch (menuItemId) {
@@ -50,21 +51,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.bottombar));
-        mBottomBar.mapColorForTab(1, ContextCompat.getColor(this, R.color.bottombar));
-        mBottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.bottombar));
-        mBottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.bottombar));
+        bottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.bottombar));
+        bottomBar.mapColorForTab(1, ContextCompat.getColor(this, R.color.bottombar));
+        bottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.bottombar));
+        bottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.bottombar));
+
+        // Load Nutrition Data from disk.
+        nutrientsTracker = new NutrientsTracker(getBaseContext(), 1 /* trimester */);
+        // Add some fake nutrition data.
+        fake_nutrition_data();
+        Log.d("MainActivity", "Total Calories: " + nutrientsTracker.getNutritionToday().calories);
+    }
+
+    private void fake_nutrition_data() {
+        if (nutrientsTracker.getNutritionToday().calories == 0) {
+            NutritionFacts pasta = new NutritionFacts("pasta", 600);
+            nutrientsTracker.log(pasta);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        nutrientsTracker.writeToFile();
+        super.onDestroy();
     }
 
     public void replaceFragment(Fragment newFragment) {
-        // TODO(prad): The highlighted item in the BottomBar should also change on reversing a transaction.
-        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        // TODO(prad): The highlighted item in the bottomBar should also change on reversing a transaction.
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.fragment_container, newFragment);
         transaction.addToBackStack(null);
         transaction.commit();
-        mFragmentManager.executePendingTransactions();
+        fragmentManager.executePendingTransactions();
     }
 
-    private BottomBar mBottomBar;
-    private FragmentManager mFragmentManager;
+    private BottomBar bottomBar;
+    private FragmentManager fragmentManager;
+    public NutrientsTracker nutrientsTracker;
 }
---
+++ mobile/src/main/java/com/group28/cs160/noms4two/NutritionFacts.java
@@ -0,0 +1,48 @@
+package com.group28.cs160.noms4two;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+/**
+ * Created by eviltwin on 4/26/16.
+ */
+public class NutritionFacts implements Serializable {
+    public String name;
+    public double calories;  // measured in kcals. Standard diet is 2000kcals.
+    public double protein;  // measured in grams.
+    public double fiber;  // measured in grams.
+    public double calcium;  // measured in milligrams.
+    public double iron;  // measured in milligrams.
+    public double potassium;  // measured in milligrams.
+    public void fromJson(JSONObject nutrition_facts) {
+        // TODO(haojun/ki): implement if necessary from an API.
+    }
+
+    public NutritionFacts(String name, double calories) {
+        this.name = name;
+        this.calories = calories;
+    }
+
+    public void add(NutritionFacts n) {
+        calories += n.calories;
+        protein += n.protein;
+        fiber += n.fiber;
+        calcium += n.calcium;
+        iron += n.iron;
+        potassium += n.potassium;
+    }
+
+    // Implement serialization.
+    // Do not change or delete this string. It will make future versions incompatible with old data.
+    private static final long serialVersionUID = 7526472295622776147L;
+    public static byte[] serialize(NutritionFacts nutritionFacts) {
+        return SerializationUtils.serialize(nutritionFacts);
+    }
+
+    public static NutritionFacts deserialize(byte[] rep_array) {
+        return (NutritionFacts) SerializationUtils.deserialize(rep_array);
+    }
+}
