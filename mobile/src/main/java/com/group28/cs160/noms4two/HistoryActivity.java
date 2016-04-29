877538be54882cb39db8a48d920ed36470c6e33b
Pradeep Kalipatnapu <prad.me@gmail.com>
2016-04-29 16:36:14 -0700

    integrate MPAndroid graph library.

---
+++ mobile/src/main/java/com/group28/cs160/noms4two/HistoryActivity.java
@@ -0,0 +1,117 @@
+package com.group28.cs160.noms4two;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryActivity extends Activity {
+
+    public static String NUTRIENT = "com.group28.cs160.NUTRIENT";
+
+    private CombinedChart mChart;
+    private final int itemcount = 12;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        mChart = (CombinedChart) findViewById(R.id.chart1);
+        mChart.setDescription("Calories");
+        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setDrawGridBackground(false);
+        mChart.setDrawBarShadow(false);
+
+        // draw bars behind lines
+        mChart.setDrawOrder(new CombinedChart.DrawOrder[] {
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.BUBBLE, CombinedChart.DrawOrder.CANDLE, CombinedChart.DrawOrder.LINE, CombinedChart.DrawOrder.SCATTER
+        });
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+
+        List<String> months = new ArrayList<String>();
+        for (int i=0; i<12; i++) {
+            months.add("Jan");
+        }
+        CombinedData data = new CombinedData(months);
+
+        data.setData(generateLineData());
+        data.setData(generateBarData());
+
+        mChart.setData(data);
+        mChart.invalidate();
+    }
+
+    private LineData generateLineData() {
+
+        LineData d = new LineData();
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        for (int index = 0; index < itemcount; index++)
+            entries.add(new Entry(getRandom(15, 10), index));
+
+        LineDataSet set = new LineDataSet(entries, "Line DataSet");
+        set.setColor(Color.rgb(240, 238, 70));
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setDrawCubic(true);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private BarData generateBarData() {
+
+        BarData d = new BarData();
+
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+
+        for (int index = 0; index < itemcount; index++)
+            entries.add(new BarEntry(getRandom(15, 30), index));
+
+        BarDataSet set = new BarDataSet(entries, "Bar DataSet");
+        set.setColor(Color.rgb(60, 220, 78));
+        set.setValueTextColor(Color.rgb(60, 220, 78));
+        set.setValueTextSize(10f);
+        d.addDataSet(set);
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        return d;
+    }
+
+    private float getRandom(float range, float startsfrom) {
+        return (float) (Math.random() * range) + startsfrom;
+    }
+}
--- mobile/src/main/java/com/group28/cs160/noms4two/NutritionFragment.java
+++ mobile/src/main/java/com/group28/cs160/noms4two/NutritionFragment.java
@@ -1,5 +1,8 @@
 package com.group28.cs160.noms4two;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.group28.cs160.shared.CenteredImageFragmentv4;
 import com.group28.cs160.shared.NutritionFacts;
 
 public class NutritionFragment extends Fragment {
@@ -23,17 +27,17 @@ public class NutritionFragment extends Fragment {
 
         MainActivity mainActivity = new MainActivity();
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, -1), R.id.caloriesCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, -1), R.id.caloriesCircle);
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, 0), R.id.proteinCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, 0), R.id.proteinCircle);
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, 1), R.id.calciumCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, 1), R.id.calciumCircle);
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, 2), R.id.fiberCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, 2), R.id.fiberCircle);
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, 3), R.id.ironCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, 3), R.id.ironCircle);
 
-        replaceFragment(GoalCircle.createGoalCircle(mainActivity, dailyGoals, dailyTotals, 4), R.id.potassiumCircle);
+        replaceFragment(createGoalCircle(getContext(), mainActivity, dailyGoals, dailyTotals, 4), R.id.potassiumCircle);
 
         return rootView;
     }
@@ -46,4 +50,84 @@ public class NutritionFragment extends Fragment {
         transaction.commit();
     }
 
+    public static CenteredImageFragmentv4 createGoalCircle(final Context context, final MainActivity activity, NutritionFacts goal, NutritionFacts info, final int position) {
+        String description;
+        double value, goalValue;
+        int border, borderHighlight;
+        int iconRes;
+        switch (position) {
+            case -1:
+                iconRes = R.drawable.calories;
+                value = info.calories;
+                goalValue = goal.calories;
+                description = "Calories";
+                border = Color.parseColor("#FFB267");
+                borderHighlight = Color.parseColor("#CC4E02");
+                break;
+            case 0:
+                iconRes = R.drawable.protein;
+                value = info.protein;
+                goalValue = goal.protein;
+                description = "Protein";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+            case 1:
+                iconRes = R.drawable.calcium;
+                value = info.calcium;
+                goalValue = goal.calcium;
+                description = "Calcium";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+            case 2:
+                iconRes = R.drawable.fiber;
+                value = info.fiber;
+                goalValue = goal.fiber;
+                description = "Fiber";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+            case 3:
+                iconRes = R.drawable.iron;
+                value = info.iron;
+                goalValue = goal.iron;
+                description = "Iron";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+            case 4:
+                iconRes = R.drawable.potassium;
+                value = info.potassium;
+                goalValue = goal.potassium;
+                description = "Potassium";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+            default:
+                iconRes = R.drawable.calories;
+                value = 0;
+                goalValue = 100;
+                description = "Not Found";
+                border = Color.parseColor("#4F751C");
+                borderHighlight = Color.parseColor("#C0FF6C");
+                break;
+        }
+        float angle = (float) (value / goalValue * 360);
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chartIntent = new Intent(context, HistoryActivity.class);
+                chartIntent.putExtra(HistoryActivity.NUTRIENT, 1);
+                context.startActivity(chartIntent);
+            }
+        };
+        CenteredImageFragmentv4 fragment = new CenteredImageFragmentv4();
+        fragment.setImage(iconRes);
+        fragment.setDescription(description);
+        fragment.setAngle(angle);
+        fragment.setColor(border, borderHighlight);
+        fragment.setOnClickListener(onClick);
+        return fragment;
+    }
 }
